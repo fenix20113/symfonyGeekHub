afc25e20398733a745232c2368017f0e37639588
Yura <yura@Mac-mini-Yura.local>
2014-11-13 09:34:50 +0200

    GeekBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,7 +19,8 @@ class AppKernel extends Kernel
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
-            $bundles[] = new Acme\DemoBundle\AcmeDemoBundle();
+//            $bundles[] = new Acme\DemoBundle\AcmeDemoBundle();
+            $bundles[] = new Geekhub\GeekBundle\GeekhubGeekBundle();
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
             $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
---
+++ src/Geekhub/GeekBundle/GeekhubGeekBundle.php
@@ -0,0 +1,15 @@
+<?php
+
+
+namespace Geekhub\GeekBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Class GeekhubGeekBundle
+ * @package Geekhub\GeekBundle
+ */
+class GeekhubGeekBundle extends Bundle
+{
+
+}
